b5329e07ca2b1453cfdf7df1b90d6f3eef7c3879
吕懿蕃 <lvyifan@lvyifandeMacBook-Pro.local>
2019-07-09 15:13:07 +0800

    update

--- Promise/index.js
+++ Promise/index.js
@@ -1,70 +1,102 @@
-let globelIndex = 0;
-class MyPromise {
-  constructor(handle) {
-    this._uid = globelIndex++;
-    if(typeof handle !== 'function') throw new Error('callback must be function')
-    this._state = 'PENDING'
-    this._value = null
-    this.successQueues = []
-    this.errorQueues = []
-    handle(this._resolve.bind(this), this._reject.bind(this));
-  }
-  _resolve(data) {
-    // console.log('_uid', this._uid);
-    this._state = 'SUCCESS'
-    this._value = data;
-    let cb
-    while (cb = this.successQueues.shift()) {
-      cb(data)
+/**
+ * 1. promise 简单的实现
+ * 2. promise then方法内，不管你是成功的回调还是失败的回调，只要返回的是正常值，那么都会在下一个then的成功回调
+ * 3. promise then方法内，你只要抛出错误那么就会在下一个then的失败回调
+ * 4. promise then方法内，如果返回值是一个promise对象，则采用他的状态
+ */
+module.exports =  class Promise {
+  constructor (executor) {
+    this.value = null;
+    this.reason = null;
+    this.state = 'PENDING';
+    this._resolve = this._resolve.bind(this);
+    this._reject = this._reject.bind(this);
+    this.successQueue = [];
+    this.errorQueue = [];
+    try {
+      executor.call(this, this._resolve, this._reject);
+    } catch (e) {
+      this._reject(e);
     }
   }
-  _reject(error) {
-    this._state = 'ERROR'
-    this._value = error
-    let cb
-    while (cb = this.errorQueues.shift()) {
-      cb(error)
+  _resolve (value) {
+    if (this.state === 'PENDING') {
+      this.value = value;
+      this.state = 'fulfilled';
+      let cb
+      while (cb = this.successQueue.shift()) {
+        cb()
+      }
     }
   }
-  then(successed, errored) {
-    let { _state, _value, successQueues, errorQueues} = this;
-    return new MyPromise((resolve, reject) => {
-      let success = (_value) => {
-        if(typeof successed !== 'function') resolve(successed)
-        else {
-          let successedResult = successed(_value);
-          if(successedResult instanceof MyPromise) {
-            successedResult.then(resolve, reject);
-          }
-          else {
-            resolve(successedResult)
-          }
-        }
+  _reject (reason) {
+    if (this.state === 'PENDING') {
+      this.reason = reason;
+      this.state = 'rejected';
+      let cb
+      while (cb = this.errorQueue.shift()) {
+        cb()
       }
-      let error = (_value) => {
-        if (typeof errored !== 'function') resolve(errored)
-        else {
-          let erroredResult = errored(_value);
-          if (erroredResult instanceof MyPromise) {
-            erroredResult.then(resolve, reject);
-          } else {
-            resolve(erroredResult)
-          }
-        }
-      }
+    }
+  }
 
-      switch (_state) {
-        case "PENDING":
-          successQueues.push(success);
-          errorQueues.push(error);
-          break;
-        case "SUCCESS":
-          success(_value)
-          break;
-        case "ERROR":
-          error(_value)
+  then (success, error) {
+    // console.log('then')
+    let { state, value, reason, successQueue, errorQueue } = this;
+    return new Promise(function (resolve, reject) {
+      let fulfilled = () => {
+        try {
+          if (typeof success !== 'function') resolve(value);
+          else {
+            let result = success(value);
+            if (result instanceof Promise) {
+              result.then(result)
+            }
+            else {
+              resolve(result)
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
+      let rejected = () => {
+        try {
+          if (typeof error !== 'function') resolve(value);
+          else {
+            let result = error(reason);
+            if (result instanceof Promise) {
+              result.then(resolve)
+            }
+            else {
+              resolve(resolve)
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
+      
+      switch (state) {
+        case 'fulfilled': fulfilled(); break;
+        case 'rejected': rejected(); break;
+        case 'PENDING': 
+          successQueue.push(fulfilled);
+          errorQueue.push(rejected);
           break;
       }
     })
   }
 }
+
+// new Promise((resolve, reject) => {
+//   resolve(1234);
+//   reject(5678);
+//   throw new Error('error');
+// }).then(data => {
+//   console.log(data);
+// }, err => {
+//   console.log(err);
+// }).then(data => {
+//   console.log(data)
+// })
--- Promise/test.js
+++ Promise/test.js
@@ -1,86 +1,103 @@
-let promise = new Promise((resolve, reject) => {
-  resolve('111');
+// class Promise {
+//   constructor (executor) {
+//     this.value = null;
+//     this.state = 'PENDING';
+//     this._resolve = this._resolve.bind(this);
+//     this._reject = this._reject.bind(this);
+//     this.successQueue = [];
+//     this.errorQueue = [];
+//     try {
+//       executor.call(this, this._resolve, this._reject);
+//     } catch (e) {
+//       this._reject(e);
+//     }
+//   }
+//   _resolve (value) {
+//     if (this.state === 'PENDING') {
+//       this.value = value;
+//       this.state = 'fulfilled';
+//       let cb
+//       while (cb = this.successQueue.shift()) {
+//         cb(this.value)
+//       }
+//     }
+//   }
+//   _reject (value) {
+//     if (this.state === 'PENDING') {
+//       this.value = value;
+//       this.state = 'rejected';
+//       let cb
+//       while (cb = this.errorQueue.shift()) {
+//         cb(this.value)
+//       }
+//     }
+//   }
+
+//   then (success, error) {
+//     // console.log('then')
+//     let { state, successQueue, errorQueue } = this;
+//     return new Promise(function (resolve, reject) {
+//       let fulfilled = (_value) => {
+//         try {
+//           if (typeof success !== 'function') resolve(_value);
+//           else {
+//             let result = success(_value);
+//             if (result instanceof Promise) {
+//               result.then(resolve)
+//             }
+//             else {
+//               resolve(result)
+//             }
+//           }
+//         } catch (error) {
+//           reject(error);
+//         }
+//       }
+//       let rejected = (_value) => {
+//         try {
+//           if (typeof error !== 'function') resolve(_value);
+//           else {
+//             let result = error(_value);
+//             if (result instanceof Promise) {
+//               result.then(resolve)
+//             }
+//             else {
+//               resolve(result)
+//             }
+//           }
+//         } catch (error) {
+//           reject(error);
+//         }
+//       }
+      
+//       switch (state) {
+//         case 'fulfilled': fulfilled(); break;
+//         case 'rejected': rejected(); break;
+//         case 'PENDING': 
+//           successQueue.push(fulfilled);
+//           errorQueue.push(rejected);
+//           break;
+//       }
+//     })
+//   }
+// }
+// const Promise = require('./Promise');
+
+new Promise((reslove, reject) => {
+  setTimeout(() => {
+    console.log('first')
+    reject(1235);
+  },1000)
 }).then((data) => {
-  console.log('1:data = ', data);
-  return '1 then result'
-}).then(data => {
-  console.log('2:data = ', data)
-})
-
-class MyPromise {
-  constructor(handler) {
-    this.status = "PENDING"
-    this.value = null
-    this.successQueue = []
-    this.errorQueue = []
-    handler(this._resolve.bind(this), yhis._reject.bind(this))
-  }
-  _resolve(value) {
-    this.status = "SUCCESS"
-    this.value = value;
-    let cb = null;
-    while (cb = this.successQueue.pop()) {
-      cb(value)
-    }
-  }
-  _reject(error) {
-    this.status = "ERROR"
-    this.value = error
-    let cb = null;
-    while (cb = this.errorQueue.pop()) {
-      cb(error)
-    }
-  }
-  then(resolveFunc, rejectFunc) {
-    return new MyPromise((resolve, reject) => {
-      let success = (value) => {
-        if(typeof resolveFunc !== 'function') {
-          resolve(resolveFunc)
-        }
-        else {
-          let runResult = resolveFunc(value);
-          if(runResult instanceof MyPromise) {
-            runResult.then(resolve, reject)
-          }
-        }
-      }
-      let error = (value) => {
-
-      }
-
-      switch (this.status) {
-        case "PENDING":
-          this.successQueue.push(success());
-          this.errorQueue.push(error())
-          break;
-        case "SUCCESS":
-
-          break;
-        case "ERROR":
-
-          break;
-      }
-    }
-    })
-}
-
-
-let promise = new MyPromise((resolve, reject) => {
-  resolve('111');
-}).then((data) => {
-  console.log('1:data = ', data);
-  return new MyPromise(resolve => {
+  return 'aaa'
+},(err) => {
+  return new Promise((resolve,reject) => {
     setTimeout(() => {
-      resolve('1 then result')
-    }, 1000);
+      resolve(err+'111');
+    }, 3000)
   })
 }).then(data => {
-  console.log('2:data = ', data)
-  return new MyPromise(resolve => {
-    setTimeout(() => {
-      resolve('2 then result')
-    }, 1000);
-  })
+  console.log(data, '---')
 }).then(data => {
-  console.log('3:data = ', data)
+  console.log(data, '++++')
 })
